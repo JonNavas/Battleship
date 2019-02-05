a655870fd1f605f1ca124e1a9127fa55a22d0cb9
JonNavas <40069322+JonNavas@users.noreply.github.com>
2019-02-04 20:43:39 -0500

    Create IShip.java

---
+++ IShip.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+/**
+ *
+ * @author patty
+ */
+public interface IShip {
+   
+    boolean isShipPlaced();
+    void setShipLocation(int row, int column);
+    int getShipLength();
+    void setShipLength(int inLength);
+    boolean isShipSunk();
+    String getShipName();
+    void setShipName(String inName);
+    int getHitsLeft();
+    int getMaxHits();
+    void setMaxHits(int inHits);
+    int getShipDirection();
+    void setShipDirection(int inDirection);
+
+}
